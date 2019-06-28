b2a7823ad73d8cd70ec468b5af832f6fe6c60fda
Paulie <xblue@protonmail.ch>
2019-06-28 11:56:51 +0300

    :apple fake-db-boilerplate

--- backend/models/Violation.js
+++ backend/models/Violation.js
@@ -2,25 +2,85 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const violationsSchema = new Schema({
-    name: {
+    firstName: {
         type: String,
         trim: true,
-        maxlength: 15
+        maxlength: 25
+    },
+    middleName: {
+        type: String,
+        trim: true,
+        maxlength: 25
+    },
+    familyName: {
+        type: String,
+        trim: true,
+        maxlength: 25
+    },
+    EGN: {
+        type: String,
+        trim: true,
+        maxlength: 25
+    },
+    LKN: {
+        type: String,
+        trim: true,
+        maxlength: 25
+    },
+    permanentAddress: {
+        type: String,
+        required: true,
+        maxlength: 50
     },
     country: {
         type: String,
         required: true,
-        maxlength: 15
+        maxlength: 20
     },
     registrationNumber: {
         type: Number,
         required: true,
         maxlength: 15
     },
+    registeredAt: {
+        type: String,
+        required: true,
+        maxlength: 25
+    },
+    beingDescription: {
+        type: String,
+        required: true,
+        maxlength: 50
+    },
+    model: {
+        type: String,
+        required: true,
+        maxlength: 20
+    },
+    brand: {
+        type: String,
+        required: true,
+        maxlength: 20
+    },
+    maximumAllowedMass: {
+        type: Number,
+        required: true,
+        maxlength: 1000
+    },
+    numberVignette: {
+        type: Number,
+        required: true,
+        maxlength: 15
+    },
+    typeOfVignette: {
+        type: String,
+        required: true,
+        maxlength: 15
+    },
     category: {
         type: String,
         required: true,
-        maxlength: 10
+        maxlength: 15
     },
     violationOwner: {
         type: String,
@@ -38,13 +98,21 @@ const violationsSchema = new Schema({
     issuedBy:{
         type: String,
         required: true,
-        maxlength: 15
+        maxlength: 25
     },
     createdBy: {
-        type: mongoose.SchemaTypes.ObjectId,
-        ref: 'users',
+        type: mongoose.SchemaTypes.ObjectId, ref: 'users',
         required: true
-    }
+    },
+    labelDescriptionA: {type: String, required: false, maxlength: 50},
+    labelDescriptionB: {type: String, required: false, maxlength: 50},
+    labelDescriptionC: {type: String, required: false, maxlength: 50},
+    labelDescriptionD: {type: String, required: false, maxlength: 50},
+    labelDescriptionE: {type: String, required: false, maxlength: 50},
+    labelDescriptionF: {type: String, required: false, maxlength: 50},
+    labelDescriptionG: {type: String, required: false, maxlength: 50},
+    labelDescriptionH: {type: String, required: false, maxlength: 50},
+
 },
 {timestamps: true}
 );
